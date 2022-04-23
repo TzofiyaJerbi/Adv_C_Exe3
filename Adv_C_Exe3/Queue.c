fdcddb8da5b7550b566b3cb1e367874581644e90
tzofiya jerbi <tzofkids1@gmail.com>
2022-04-23 20:26:49 +0300

    חלק מתורים. לסיים פונקציות

--- Adv_C_Exe3/Queue.c
+++ Adv_C_Exe3/Queue.c
@@ -1,20 +1,45 @@
+#define CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "Queue.h"
 
 /***************** Queue ADT Implementation *****************/
 
 void initQueue(Queue* q)
 {
-	// add your code here
+
+	q->head = NULL;
+	q->tail == NULL;
+
 }
 
 void destroyQueue(Queue* q)
 {
-	// add your code here
+	int e; // empti
+	e = isEmptyQueue;
+	if (e==1)
+	//while (!isEmptyQueue(q))  אופציה לפונקציה בלי משתנה
+		dequeue(q);
+	free(q);
 }
 
 void enqueue(Queue* q, unsigned int data)
 {
-	// add your code here
+
+	intNode* newToLest = (intNode*)calloc(1, sizeof(intNode));
+	if (newToLest != NULL) {
+		newToLest->data = data;
+		if (!isEmptyQueue(q))
+		{
+			q->tail->next = newToLest;
+			q->tail = newToLest;
+			return;
+		}
+		q->tail = q->head = newToLest;
+	}
+	else
+		return;
 }
 
 unsigned int dequeue(Queue* q)
@@ -24,7 +49,11 @@ unsigned int dequeue(Queue* q)
 
 int isEmptyQueue(const Queue* q)
 {
-	// add your code here
+
+	if (q->head == NULL && q->tail == NULL)
+		return 1;
+	return 0;
+
 }
 
 /***************** Functions using Queues - Implementation/definition **************************/
